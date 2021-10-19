91ee09bebaae3825f8bed98bc42c8fc9544d9f8e
Jeff Albrecht <geospatialjeff@gmail.com>
2021-10-18 22:52:55 -0600

    fetch test data before each pytest session

---
+++ tests/conftest.py
@@ -0,0 +1,29 @@
+import os
+
+import pytest
+from pystac_client import Client as StacApiClient
+
+from .utils import create_single_file_stac
+
+PLANETARY_COMPUTER_URL = os.getenv(
+    "PLANETARY_COMPUTER_URL", "https://planetarycomputer.microsoft.com/api/stac/v1"
+)
+
+
+@pytest.fixture(scope="session")
+def pc_client() -> StacApiClient:
+    return StacApiClient.open(url=PLANETARY_COMPUTER_URL)
+
+
+@pytest.fixture(scope="session")
+def get_test_data(pc_client):
+    return create_single_file_stac(
+        collection_id="naip",
+        bbox=(
+            -118.40738296508789,
+            34.0137518391494,
+            -118.3915901184082,
+            34.025205466778296,
+        ),
+        stac_api_client=pc_client,
+    )
